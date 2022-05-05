7c1a7fb7b73a0e61f83c6bd69c56458a3f62cca2
thedarklordzw <swiftly.graphics@gmail.com>
2022-05-05 19:33:04 +0200

    v for vercel

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,9 +1,10 @@
 import styles from './MeetupDetail.module.css';
+import Image from 'next/image';
 
 function MeetupDetail(props) {
   return (
     <section className={styles.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
